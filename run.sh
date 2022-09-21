216805b647f7e7afcc2dea20a8573ccde111eb1d
ak-alam <alam.akbar1989@gmail.com>
2022-09-21 20:25:51 +0500

    gunicorn path

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 cd app
 source .env/bin/activate
 pip3 install -r requirements.txt
-gunicorn -b 0.0.0.0:5000 -w=2 wsgi:app
+# gunicorn -b 0.0.0.0:5000 -w=2 wsgi:app
+/home/ec2-user/.local/bin/gunicorn -b 0.0.0.0:5000 -w=2 wsgi:app
